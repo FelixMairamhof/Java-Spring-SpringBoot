9b99a0dfc608ed43afd31c3863dc89d7265d2e66
Felix Mairamhof <feberg.k@gmail.com>
2024-05-03 17:38:33 +0200

    create new beans and classes with record

--- learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/App2HelloWorldSpring.java
+++ learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/App2HelloWorldSpring.java
@@ -2,6 +2,7 @@ package com.in28minutes.learnspringframework;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+
 public class App2HelloWorldSpring {
     public static void main(String[] args) {
         //1: Launch a Spring Context
@@ -15,6 +16,16 @@ public class App2HelloWorldSpring {
         //3: Retrieving Beans managed by Spring
         var name = context.getBean("name");
         System.out.println(name);
+
+        var age = context.getBean("age");
+        System.out.println(age);
+
+        var person = context.getBean("person");
+        System.out.println(person);
+
+        var address = context.getBean("address");
+        System.out.println(address);
+
 
 
     }
--- learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/HelloWorlConfiguration.java
+++ learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/HelloWorlConfiguration.java
@@ -3,10 +3,30 @@ package com.in28minutes.learnspringframework;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+//Eliminate verbosity of jave
+//Public accessor mehtods, constructer, equals, hashcode and toString
+//are automaticly created new in JDK 16
+record Person (String name, int age){};
+record Address (String firstLine, String city){};
+
 @Configuration
 public class HelloWorlConfiguration {
     @Bean
     public String name (){
         return "Hello World";
+    }
+    @Bean
+    public  int age (){
+        return 22;
+    }
+    @Bean
+    public Person person(){
+        var person = new Person("John", 22);
+        return person;
+    }
+    @Bean
+    public Address address(){
+        var address = new Address("Some Street", "Some City");
+        return address;
     }
 }
