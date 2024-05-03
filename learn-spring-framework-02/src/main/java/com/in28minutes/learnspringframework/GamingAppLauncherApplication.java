6d2d9dc44fae0284e7118e8f9c961f7ad7f4152f
Felix Mairamhof <feberg.k@gmail.com>
2024-05-03 21:35:07 +0200

    Auto wire components for all games and change priority with quilifier or primary

--- learn-spring-framework-02/src/main/java/com/in28minutes/learnspringframework/GamingAppLauncherApplication.java
+++ learn-spring-framework-02/src/main/java/com/in28minutes/learnspringframework/GamingAppLauncherApplication.java
@@ -2,26 +2,21 @@ package com.in28minutes.learnspringframework;
 
 import com.in28minutes.learnspringframework.game.GameRunner;
 import com.in28minutes.learnspringframework.game.GamingConsole;
-import com.in28minutes.learnspringframework.game.PacManGame;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 
-import com.in28minutes.learnspringframework.game.GameRunner;
-import com.in28minutes.learnspringframework.game.GamingConsole;
-import com.in28minutes.learnspringframework.game.PacManGame;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 
 @Configuration
 @ComponentScan("com.in28minutes.learnspringframework.game")
-public class App3GamingSpringBeans {
+public class GamingAppLauncherApplication {
 
     public static void main(String[] args) {
         try (var context =
                      new AnnotationConfigApplicationContext
-                             (App3GamingSpringBeans.class)){
+                             (GamingAppLauncherApplication.class)){
 
             context.getBean(GamingConsole.class).up();
 
