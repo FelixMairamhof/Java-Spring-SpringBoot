e520e42554d8d449c550833cefb9a7e0300a1eba
Felix Mairamhof <feberg.k@gmail.com>
2024-05-05 17:06:41 +0200

    Dependency Injection Setter, Field, Constructor

--- learn-spring-framework-02/src/main/java/com/in28minutes/learnspringframework/examples/a1/DepInjectionLauncherApplication.java
+++ learn-spring-framework-02/src/main/java/com/in28minutes/learnspringframework/examples/a1/DepInjectionLauncherApplication.java
@@ -1,11 +1,50 @@
 package com.in28minutes.learnspringframework.examples.a1;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
+
+@Component
+class YourBuisnessClass{
+    //@autowired = Field injection
+    Dependency1 dependency1;
+    //@autowired = Field injection
+    Dependency2 dependency2;
+
+    @Autowired //Constructor Injection but works without Autowired
+    public YourBuisnessClass(Dependency1 dependency1, Dependency2 dependency2) {
+        this.dependency1 = dependency1;
+        this.dependency2 = dependency2;
+    }
+
+    //@Autowired = SetterInjection
+    public void setDependency1(Dependency1 dependency1) {
+        this.dependency1 = dependency1;
+    }
+    //@Autowired = SetterInjection
+    public void setDependency2(Dependency2 dependency2) {
+        this.dependency2 = dependency2;
+    }
+
+    @Override
+    public String toString() {
+        return "dependency1=" + dependency1 + ", dependency2=" + dependency2;
+    }
+}
+@Component
+class Dependency1{
+
+}
+@Component
+class Dependency2{
+
+}
+
 
 
 @Configuration
@@ -19,9 +58,7 @@ public class DepInjectionLauncherApplication {
 
             Arrays.stream(context.getBeanDefinitionNames())
                     .forEach(System.out::println);
-
+            System.out.println(context.getBean(YourBuisnessClass.class));
         }
-
-
     }
 }
